0baef71f402650f6af3a9fde41e3de67b7895773
Christopher Kumm <chrislkumm@protonmail.com>
2025-11-15 17:36:50 -0600

    Add NPC scene, improve movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,9 +1,10 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed := 60
 
 var direction := Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
